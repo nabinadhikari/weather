3c82ba8ed4945c1a8694a9193c85dcd0e8b3ac56
Nabin Adhikari <mr.nabinadhikari@gmail.com>
2022-08-06 15:04:15 +1000

    added test

---
+++ Weather.Api.Tests/WeatherControllerTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Weather.Api.Tests;
+
+public class WeatherControllerTests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Weather.Api/Controllers/WeatherController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Weather.Api.Controllers;
+
+[ApiController]
+[Route("weather")]
+public class WeatherController : ControllerBase
+{
+    private readonly ILogger<WeatherController> _logger;
+
+    public WeatherController(ILogger<WeatherController> logger)
+    {
+        _logger = logger;
+    }
+}
