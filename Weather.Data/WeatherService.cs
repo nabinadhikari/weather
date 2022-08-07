473cb1d13c7c485b2f2702f50dcac1d180d9492e
Nabin Adhikari <mr.nabinadhikari@gmail.com>
2022-08-07 10:03:28 +1000

    random api keys

--- Weather.Data/WeatherService.cs
+++ Weather.Data/WeatherService.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Weather.Contracts;
 
 namespace Weather.Data;
@@ -11,9 +10,21 @@ public class WeatherService : IWeatherService
     {
         _http = new HttpClient();
     }
+
+    private string GetApiKey()
+    {
+        var apiKeys = new string[] {
+            "8b7535b42fe1c551f18028f64e8688f7",
+            "9f933451cebf1fa39de168a29a4d9a79"
+        };
+        var random = new Random();
+        var randomIndex = random.Next(apiKeys.Length);
+        return apiKeys[randomIndex];
+    }
+
     public async Task<ApiResult> GetWeatherForCityAsync(string city, string country)
     {
-        var url = $"https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid=8b7535b42fe1c551f18028f64e8688f7";
+        var url = $"https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={GetApiKey()}";
         try
         {
             var response = await _http.GetAsync(url);
